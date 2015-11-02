602fc4d21c18daa3c63d1445f74be5335a1f713b
Jan Sedlák <jsedlak@redhat.com>
2015-11-02 15:31:53 +0100

    use values instead of pointers in methods, pointers are not necessary

--- matrices.go
+++ matrices.go
@@ -9,54 +9,60 @@ import (
 
 // Matrix represents two-dimensional field
 type Matrix struct {
-    Rows, Cols int
+    cols int
     values []float64
 }
 
-// Shape returns dimensions of matrix in slice
-func (m *Matrix) Shape() []int {
-    return []int{m.Rows, m.Cols}
+// Rows returns number of rows in matrix
+func (m Matrix) Rows() int {
+    return len(m.values)/m.cols
+}
+
+// Cols returns number of columns in matrix
+func (m Matrix) Cols() int {
+    return m.cols
 }
 
 // InitMatrix initializes Matrix structure to have required number of rows and columns
-func InitMatrix(rows, cols int) *Matrix {
-    m := Matrix{Rows: rows, Cols: cols}
+func InitMatrix(rows, cols int) Matrix {
+    m := Matrix{cols: cols}
     m.values = make([]float64, rows*cols)
-    return &m
+    return m
 }
 
-// RandInitMatrix initializes Matrix structure and fills it with random numbers from random generator
-func RandInitMatrix(rows, cols int, r *rand.Rand) *Matrix {
+// RandInitMatrix initializes Matrix structure and fills it with random numbers
+func RandInitMatrix(rows, cols int) Matrix {
     m := InitMatrix(rows, cols)
-    for i := 0; i < rows * cols; i++ {
-        m.values[i] = r.Float64()
+    for i := range m.values {
+        m.values[i] = rand.Float64()
     }
     return m
 }
 
 // InitMatrixWithValues initializes Matrix with given dimensions and values
-func InitMatrixWithValues(rows, cols int, values []float64) (*Matrix, error) {
+func InitMatrixWithValues(rows, cols int, values []float64) (Matrix, error) {
+    var m Matrix
     if len(values) != rows*cols {
-        return nil, errors.New("matrices: bad dimensions of matrix")
+        return m, errors.New("matrices: bad dimensions of matrix")
     }
-    m := Matrix{Rows: rows, Cols: cols, values: values}
-    return &m, nil
+    m = Matrix{cols: cols, values: values}
+    return m, nil
 }
 
-func (m *Matrix) checkRowCol(row, col int) bool {
-    return row < m.Rows && col < m.Cols && row >= 0 && col >= 0
+func (m Matrix) checkRowCol(row, col int) bool {
+    return row < m.Rows() && col < m.Cols() && row >= 0 && col >= 0
 }
 
-func (m *Matrix) at(row, col int) float64 {
-    return m.values[row * m.Cols + col]
+func (m Matrix) at(row, col int) float64 {
+    return m.values[row * m.Cols() + col]
 }
 
-func (m *Matrix) set(row, col int, value float64) {
-    m.values[row * m.Cols + col] = value
+func (m Matrix) set(row, col int, value float64) {
+    m.values[row * m.Cols() + col] = value
 }
 
 // At returns item that is in matrix at given coordinates
-func (m *Matrix) At(row, col int) (float64, error) {
+func (m Matrix) At(row, col int) (float64, error) {
     if !m.checkRowCol(row, col) {
         return 0, errors.New("matrices: cannot get value outside of matrix")
     }
@@ -64,7 +70,7 @@ func (m *Matrix) At(row, col int) (float64, error) {
 }
 
 // Set sets item in matrix to given value
-func (m *Matrix) Set(row, col int, value float64) error {
+func (m Matrix) Set(row, col int, value float64) error {
     if !m.checkRowCol(row, col) {
         return errors.New("matrices: cannot set value outside of matrix")
     }
@@ -72,11 +78,12 @@ func (m *Matrix) Set(row, col int, value float64) error {
     return nil
 }
 
-func (m *Matrix) operate(n *Matrix, operation func(float64, float64) float64) (*Matrix, error) {
-    if m.Rows != n.Rows || m.Cols != n.Cols {
-        return nil, errors.New("matrices: operating on two matrices with different dimensions")
+func (m Matrix) operate(n Matrix, operation func(float64, float64) float64) (Matrix, error) {
+    var result Matrix
+    if m.Rows() != n.Rows() || m.Cols() != n.Cols() {
+        return result, errors.New("matrices: operating on two matrices with different dimensions")
     }
-    result := InitMatrix(m.Rows, m.Cols)
+    result = InitMatrix(m.Rows(), m.Cols())
     for i := range m.values {
         result.values[i] = operation(m.values[i], n.values[i])
     }
@@ -84,18 +91,18 @@ func (m *Matrix) operate(n *Matrix, operation func(float64, float64) float64) (*
 }
 
 // Add adds two matrices
-func (m *Matrix) Add(n *Matrix) (*Matrix, error) {
+func (m Matrix) Add(n Matrix) (Matrix, error) {
     return m.operate(n, func (x, y float64) float64 { return x + y; })
 }
 
 // Sub subtracts two matrices
-func (m *Matrix) Sub(n *Matrix) (*Matrix, error) {
+func (m Matrix) Sub(n Matrix) (Matrix, error) {
     return m.operate(n, func (x, y float64) float64 { return x - y; })
 }
 
 // Apply applies function to each element of Matrix
-func (m *Matrix) Apply(operation func(float64) float64) *Matrix {
-    result := InitMatrix(m.Rows, m.Cols)
+func (m Matrix) Apply(operation func(float64) float64) Matrix {
+    result := InitMatrix(m.Rows(), m.Cols())
     for i, val := range m.values {
         result.values[i] = operation(val)
     }
@@ -103,15 +110,16 @@ func (m *Matrix) Apply(operation func(float64) float64) *Matrix {
 }
 
 // MatrixMult multiplies two matrices
-func (m *Matrix) MatrixMult(n *Matrix) (*Matrix, error) {
-    if m.Cols != n.Rows {
-        return nil, errors.New("matrices: for matrix multiplication, first matrix cols == second matrix rows")
+func (m Matrix) MatrixMult(n Matrix) (Matrix, error) {
+    var result Matrix
+    if m.Cols() != n.Rows() {
+        return result, errors.New("matrices: for matrix multiplication, first matrix cols == second matrix rows")
     }
-    result := InitMatrix(m.Rows, n.Cols)
-    for i := 0; i < result.Rows; i++ {
-        for j := 0; j < result.Cols; j++ {
+    result = InitMatrix(m.Rows(), n.Cols())
+    for i := 0; i < result.Rows(); i++ {
+        for j := 0; j < result.Cols(); j++ {
             sum := 0.0
-            for counter := 0; counter < m.Cols; counter++ {
+            for counter := 0; counter < m.Cols(); counter++ {
                 sum += m.at(i, counter) * n.at(counter, j)
             }
             result.set(i, j, sum)
@@ -121,10 +129,10 @@ func (m *Matrix) MatrixMult(n *Matrix) (*Matrix, error) {
 }
 
 // Transpose creates transposed matrix of original matrix
-func (m *Matrix) Transpose() *Matrix {
-    result := InitMatrix(m.Cols, m.Rows)
-    for i := 0; i < m.Rows; i++ {
-        for j := 0; j < m.Cols; j++ {
+func (m Matrix) Transpose() Matrix {
+    result := InitMatrix(m.Cols(), m.Rows())
+    for i := 0; i < m.Rows(); i++ {
+        for j := 0; j < m.Cols(); j++ {
             val := m.at(i, j)
             result.set(j, i, val)
         }
@@ -133,8 +141,8 @@ func (m *Matrix) Transpose() *Matrix {
 }
 
 // Max returns biggest value in matrix
-func (m *Matrix) Max() (float64, error) {
-    if m.Rows == 0 || m.Cols == 0 {
+func (m Matrix) Max() (float64, error) {
+    if m.Rows() == 0 || m.Cols() == 0 {
         return 0, errors.New("matrices: can't return max value in empty matrix")
     }
     maxval := m.values[0]
@@ -147,8 +155,8 @@ func (m *Matrix) Max() (float64, error) {
 }
 
 // Min returns smallest value in matrix
-func (m *Matrix) Min() (float64, error) {
-    if m.Rows == 0 || m.Cols == 0 {
+func (m Matrix) Min() (float64, error) {
+    if m.Rows() == 0 || m.Cols() == 0 {
         return 0, errors.New("matrices: can't return min value in empty matrix")
     }
     minval := m.values[0]
@@ -161,18 +169,15 @@ func (m *Matrix) Min() (float64, error) {
 }
 
 // String converts matrix to string
-func (m *Matrix) String() (result string) {
-    if m.Rows == 0 || m.Cols == 0 {
-        return "empty Matrix"
-    }
+func (m Matrix) String() (result string) {
     maxval, _ := m.Max()
     flen := numberlen(maxval)
     floatfmt := fmt.Sprintf("%%%d.2f", flen + 6)
-    rows := make([]string, m.Rows)
+    rows := make([]string, m.Rows())
 
-    for i := 0; i < m.Rows; i++ {
+    for i := 0; i < m.Rows(); i++ {
         row := ""
-        for j := 0; j < m.Cols; j++ {
+        for j := 0; j < m.Cols(); j++ {
             val := m.at(i, j)
             row += fmt.Sprintf(floatfmt, val)
         }
--- vectors.go
+++ vectors.go
@@ -7,108 +7,108 @@ import (
 )
 
 // Vector represends one-dimensional field
-type Vector struct {
-    Length int
-    values []float64
+type Vector []float64
+
+// Rows returns number of rows in vector - 1
+func (v Vector) Rows() int {
+    return 1
 }
 
-// Shape returns returns length of vector
-func (v *Vector) Shape() []int {
-    return []int{v.Length}
+// Cols returns number of columns in vector
+func (v Vector) Cols() int {
+    return len(v)
 }
 
 // InitVector creates new vector with given length
-func InitVector(length int) *Vector {
-    v := Vector{Length: length}
-    v.values = make([]float64, length)
-    return &v
+func InitVector(length int) Vector {
+    return Vector(make([]float64, length))
 }
 
 // InitVectorWithValues creates new vector with given values
-func InitVectorWithValues(values []float64) *Vector {
-    v := Vector{Length: len(values), values: values}
-    return &v
+func InitVectorWithValues(values []float64) Vector {
+    return Vector(values)
 }
 
 // RandInitVector creates new vector with given length and random values
-func RandInitVector(length int, r *rand.Rand) *Vector {
+func RandInitVector(length int) Vector {
     v := InitVector(length)
-    for i := range v.values {
-        v.values[i] = r.Float64()
+    for i := range v {
+        v[i] = rand.Float64()
     }
     return v
 }
 
-func (v *Vector) checkIndex(index int) bool {
-    return index >= 0 && index < v.Length
+func (v Vector) checkIndex(index int) bool {
+    return index >= 0 && index < len(v)
 }
 
 // At returns item that is in vector at given index
-func (v *Vector) At(i int) (float64, error) {
+func (v Vector) At(i int) (float64, error) {
     if !v.checkIndex(i) {
         return 0, errors.New("matrices: cannot get value outside of vector")
     }
-    return v.values[i], nil
+    return v[i], nil
 }
 
 // Set sets item in vector to given value
-func (v *Vector) Set(i int, value float64) error {
+func (v Vector) Set(i int, value float64) error {
     if !v.checkIndex(i) {
         return errors.New("matrices: cannot set value outside of vector")
     }
-    v.values[i] = value
+    v[i] = value
     return nil
 }
 
-func (v *Vector) operate(w *Vector, operation func(float64, float64) float64) (*Vector, error) {
-    if v.Length != w.Length {
-        return nil, errors.New("matrices: operating on two vectors with different lengths")
+func (v Vector) operate(w Vector, operation func(float64, float64) float64) (Vector, error) {
+    var result Vector
+    if len(v) != len(w) {
+        return result, errors.New("matrices: operating on two vectors with different lengths")
     }
-    result := InitVector(v.Length)
-    for i := range result.values {
-        result.values[i] = operation(v.values[i], w.values[i])
+    result = InitVector(len(v))
+    for i := range result {
+        result[i] = operation(v[i], w[i])
     }
     return result, nil
 }
 
 // Add adds two vectors
-func (v *Vector) Add(w *Vector) (*Vector, error) {
+func (v Vector) Add(w Vector) (Vector, error) {
     return v.operate(w, func (x, y float64) float64 { return x + y; })
 }
 
 // Sub subtracts two vectors
-func (v *Vector) Sub(w *Vector) (*Vector, error) {
+func (v Vector) Sub(w Vector) (Vector, error) {
     return v.operate(w, func (x, y float64) float64 { return x - y; })
 }
 
 // Dot returns dot product of two vectors
-func (v *Vector) Dot(w *Vector) (float64, error) {
-    if v.Length != w.Length {
+func (v Vector) Dot(w Vector) (float64, error) {
+    if len(v) != len(w) {
         return 0, errors.New("matrices: cannot do dot product of vectors with different sizes")
     }
     sum := float64(0)
-    for i := range v.values {
-        sum += v.values[i] * w.values[i]
+    for i := range v {
+        sum += v[i] * w[i]
     }
     return sum, nil
 }
 
 // Apply applies function to each element of vector
-func (v *Vector) Apply(operation func(float64) float64) *Vector {
-    result := InitVector(v.Length)
-    for i, val := range v.values {
-        result.values[i] = operation(val)
+func (v Vector) Apply(operation func(float64) float64) Vector {
+    result := InitVector(len(v))
+    for i, val := range v {
+        result[i] = operation(val)
     }
     return result
 }
 
 // Max returns biggest value in vector
-func (v *Vector) Max() (float64, error) {
-    if v.Length == 0 {
+func (v Vector) Max() (float64, error) {
+    if len(v) == 0 {
         return 0, errors.New("matrices: can't get max value in empty vector")
     }
-    maxval := v.values[0]
-    for _, val := range v.values {
+    maxval := v[0]
+    for _, val := range v {
         if val > maxval {
             maxval = val
         }
@@ -117,12 +117,12 @@ func (v *Vector) Max() (float64, error) {
 }
 
 // Min returns smallest value in vector
-func (v *Vector) Min() (float64, error) {
-    if v.Length == 0 {
+func (v Vector) Min() (float64, error) {
+    if len(v) == 0 {
         return 0, errors.New("matrices: can't get min value in empty vector")
     }
-    minval := v.values[0]
-    for _, val := range v.values {
+    minval := v[0]
+    for _, val := range v {
         if val < minval {
             minval = val
         }
@@ -130,14 +130,14 @@ func (v *Vector) Min() (float64, error) {
     return minval, nil
 }
 
-func (v *Vector) String() (result string) {
-    if v.Length == 0 {
+func (v Vector) String() (result string) {
+    if len(v) == 0 {
         return "empty Vector"
     }
     maxval, _ := v.Max()
     flen := numberlen(maxval)
     floatfmt := fmt.Sprintf("%%%d.2f", flen + 6)
-    for _, val := range v.values {
+    for _, val := range v {
         result += fmt.Sprintf(floatfmt, val)
     }
     result = "[ " + result + " ]"
